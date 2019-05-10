6e5f0024819c7086fe6845fe946988b25e8ac9a8
Eltonmc <eltonmc@dcomp.ufs.br>
2019-05-10 15:57:16 -0300

    fix layout

--- src/app/pages/introduction/introduction.page.ts
+++ src/app/pages/introduction/introduction.page.ts
@@ -15,7 +15,8 @@ export class IntroductionPage implements OnInit {
     on: {
       beforeInit() {
         const swiper = this;
-        swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
+        swiper.classNames.push(`${swiper.params.containerModifierClass}flip`);
+        swiper.classNames.push(`${swiper.params.containerModifierClass}3d`);
         const overwriteParams = {
           slidesPerView: 1,
           slidesPerColumn: 1,
@@ -25,40 +26,67 @@ export class IntroductionPage implements OnInit {
           virtualTranslate: true,
         };
         swiper.params = Object.assign(swiper.params, overwriteParams);
-        swiper.params = Object.assign(swiper.originalParams, overwriteParams);
+        swiper.originalParams = Object.assign(swiper.originalParams, overwriteParams);
       },
       setTranslate() {
         const swiper = this;
-        const { slides } = swiper;
+        const { $, slides, rtlTranslate: rtl } = swiper;
         for (let i = 0; i < slides.length; i += 1) {
-          const $slideEl = swiper.slides.eq(i);
+          const $slideEl = slides.eq(i);
+          let progress = $slideEl[0].progress;
+          if (swiper.params.flipEffect.limitRotation) {
+            progress = Math.max(Math.min($slideEl[0].progress, 1), -1);
+          }
           const offset$$1 = $slideEl[0].swiperSlideOffset;
+          const rotate = -180 * progress;
+          let rotateY = rotate;
+          let rotateX = 0;
           let tx = -offset$$1;
-          if (!swiper.params.virtualTranslate) tx -= swiper.translate;
           let ty = 0;
           if (!swiper.isHorizontal()) {
             ty = tx;
             tx = 0;
+            rotateX = -rotateY;
+            rotateY = 0;
+          } else if (rtl) {
+            rotateY = -rotateY;
           }
-          const slideOpacity = swiper.params.fadeEffect.crossFade
-            ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
-            : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
+  
+           $slideEl[0].style.zIndex = -Math.abs(Math.round(progress)) + slides.length;
+  
+           if (swiper.params.flipEffect.slideShadows) {
+            // Set shadows
+            let shadowBefore = swiper.isHorizontal() ? $slideEl.find('.swiper-slide-shadow-left') : $slideEl.find('.swiper-slide-shadow-top');
+            let shadowAfter = swiper.isHorizontal() ? $slideEl.find('.swiper-slide-shadow-right') : $slideEl.find('.swiper-slide-shadow-bottom');
+            if (shadowBefore.length === 0) {
+              shadowBefore = $(`<div class="swiper-slide-shadow-${swiper.isHorizontal() ? 'left' : 'top'}"></div>`);
+              $slideEl.append(shadowBefore);
+            }
+            if (shadowAfter.length === 0) {
+              shadowAfter = $(`<div class="swiper-slide-shadow-${swiper.isHorizontal() ? 'right' : 'bottom'}"></div>`);
+              $slideEl.append(shadowAfter);
+            }
+            if (shadowBefore.length) shadowBefore[0].style.opacity = Math.max(-progress, 0);
+            if (shadowAfter.length) shadowAfter[0].style.opacity = Math.max(progress, 0);
+          }
           $slideEl
-            .css({
-              opacity: slideOpacity,
-            })
-            .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
+            .transform(`translate3d(${tx}px, ${ty}px, 0px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
         }
       },
       setTransition(duration) {
         const swiper = this;
-        const { slides, $wrapperEl } = swiper;
-        slides.transition(duration);
+        const { slides, activeIndex, $wrapperEl } = swiper;
+        slides
+          .transition(duration)
+          .find('.swiper-slide-shadow-top, .swiper-slide-shadow-right, .swiper-slide-shadow-bottom, .swiper-slide-shadow-left')
+          .transition(duration);
         if (swiper.params.virtualTranslate && duration !== 0) {
           let eventTriggered = false;
-          slides.transitionEnd(() => {
+          // eslint-disable-next-line
+          slides.eq(activeIndex).transitionEnd(function onTransitionEnd() {
             if (eventTriggered) return;
             if (!swiper || swiper.destroyed) return;
+  
             eventTriggered = true;
             swiper.animating = false;
             const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
@@ -67,9 +95,9 @@ export class IntroductionPage implements OnInit {
             }
           });
         }
-      },
+      }
     }
-  }
+  };
 
   constructor() { }
 
