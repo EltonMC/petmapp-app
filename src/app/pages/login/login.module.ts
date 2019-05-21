1f7255abd094c26f504638625285b1ea401d8625
Eltonmc <eltonmc@dcomp.ufs.br>
2019-05-21 10:53:45 -0300

    add login functions

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 const routes: Routes = [
   {
@@ -19,7 +21,9 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    ComponentsModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+// import { RegisterPage } from '../register/register.page';
+import { NgForm, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertService } from 'src/app/services/alert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  submitFail = false;
+  loginForm: any;
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController,
+    private authService: AuthService,
+    private alertService: AlertService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.maxLength(255), Validators.email]],
+      password: [null,[Validators.required, Validators.minLength(3), Validators.maxLength(255)]]
+    });
   }
+  // Dismiss Login Modal
+  // dismissLogin() {
+  //   this.modalController.dismiss();
+  // }
 
+  // On Register button tap, dismiss login modal and open register modal
+  // async registerModal() {
+  //   this.dismissLogin();
+  //   const registerModal = await this.modalController.create({
+  //     component: RegisterPage
+  //   });
+  //   return await registerModal.present();
+  // }
+
+  submitForm(form) {
+    this.authService.login(form.value.email, form.value.password).subscribe(
+      data => {
+
+      },
+      error => {
+        if (error.status === 400) {
+          this.submitFail = true;
+        }
+      },
+      () => {
+        this.router.navigate(['/menu/home']);
+
+      }
+    );
+  }
 }
