41a65b07d9d8215ac74f777cecc45b8bd77392a2
Eltonmc <eltonmc@dcomp.ufs.br>
2019-05-21 10:54:17 -0300

    services init

---
+++ src/app/services/env.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvService {
+//   API_URL = 'https://pet-mapp.herokuapp.com/';
+    API_URL = 'http://localhost:8100/';
+    constructor() { }
+}
