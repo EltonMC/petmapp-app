da741989883519cd261e517aae996b2ed7a0d899
Eltonmc <eltonmc@dcomp.ufs.br>
2019-05-21 10:53:11 -0300

    add auth guard on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
@@ -12,16 +13,17 @@ const routes: Routes = [
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
   { path: 'remember-password', loadChildren: './pages/remember-password/remember-password.module#RememberPasswordPageModule' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
-  { path: 'configuration', loadChildren: './pages/configuration/configuration.module#ConfigurationPageModule' },
-  { path: 'perfil', loadChildren: './pages/perfil/perfil.module#PerfilPageModule' },
-  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
-  { path: 'reservation-list', loadChildren: './pages/reservation-list/reservation-list.module#ReservationListPageModule' },
-  { path: 'pet-list', loadChildren: './pages/pet-list/pet-list.module#PetListPageModule' },
-  { path: 'pet', loadChildren: './pages/pet/pet.module#PetPageModule' },
-  { path: 'reservation', loadChildren: './pages/reservation/reservation.module#ReservationPageModule' },
-  { path: 'petshop', loadChildren: './pages/petshop/petshop.module#PetshopPageModule' }
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule', canActivate: [AuthGuard] },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule', canActivate: [AuthGuard] },
+  { path: 'configuration', loadChildren: './pages/configuration/configuration.module#ConfigurationPageModule', canActivate: [AuthGuard] },
+  { path: 'perfil', loadChildren: './pages/perfil/perfil.module#PerfilPageModule', canActivate: [AuthGuard] },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule', canActivate: [AuthGuard] },
+  { path: 'reservation-list', loadChildren: './pages/reservation-list/reservation-list.module#ReservationListPageModule', 
+    canActivate: [AuthGuard] },
+  { path: 'pet-list', loadChildren: './pages/pet-list/pet-list.module#PetListPageModule', canActivate: [AuthGuard] },
+  { path: 'pet', loadChildren: './pages/pet/pet.module#PetPageModule', canActivate: [AuthGuard] },
+  { path: 'reservation', loadChildren: './pages/reservation/reservation.module#ReservationPageModule', canActivate: [AuthGuard] },
+  { path: 'petshop', loadChildren: './pages/petshop/petshop.module#PetshopPageModule', canActivate: [AuthGuard] }
 ];
 
 @NgModule({
