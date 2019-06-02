1867c3cfc890a2924bb5d86eaa9cfd32e51bb630
Eltonmc <eltonmc@dcomp.ufs.br>
2019-06-02 19:08:32 -0300

    update petshop model

--- src/app/models/petshop.ts
+++ src/app/models/petshop.ts
@@ -2,22 +2,33 @@ export class Petshop {
     id: number;
     name: string;
     description: string;
+    logo: string;
+    max_discount: number;
+    phone:[{
+        phone: string;
+    }];
+    num_services: number;
+    rating_average: number;
+    schedule: string;
     images: [{
         image: string,
         status: boolean
     }];
+    address: {
+        id: string,
+        cep: string,
+        state: string,
+        city: string,
+        district: string,
+        street: string,
+        number: string,
+        complement: string,
+        longitude: string,
+        latitude: string
+    };
     services: [{
         id: string,
         type: string,
         status: boolean,
-        turns: [{
-            id: string,
-            day: string,
-            time_start: string,
-            time_end: string,
-            max_reservations: number,
-            percent_discount: number,
-            status: boolean
-        }]
     }];
 }
