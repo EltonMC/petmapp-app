ea42a612a56d554bea9422df5dacd915ec4962bd
Eltonmc <eltonmc@dcomp.ufs.br>
2019-06-06 16:20:23 -0300

    add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EnvService } from './env.service';
+import { User } from '../models/user';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService {
+
+    private user: User;
+    
+    constructor(
+        private httpClient: HttpClient,
+        private env: EnvService,
+    ) {}
+
+    private set(user: User) {
+        this.user = user;
+    }
+
+    async get() {
+        if(!this.user){
+            const { data } = await this.load();
+            return data;
+        }   
+        return this.user;
+    }
+
+    load(){
+        return this.httpClient.get<any>(this.env.API_URL + 'users').toPromise();
+    }
+
+    update(user: User){
+        return this.httpClient.put<User>(this.env.API_URL + 'users', user).pipe(
+            tap(res => {
+                this.set(res.data);
+                return res;
+            })
+        );
+    }
+
+    register(){
+
+    }
+}
