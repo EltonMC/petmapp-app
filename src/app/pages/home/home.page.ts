d7c7730266c1db81173ae55de70e0349e961e3d9
Eltonmc <eltonmc@dcomp.ufs.br>
2019-06-07 15:02:05 -0300

    fix display name

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,8 @@ import { PetshopService } from './../../services/petshop.service';
 import { Component, OnInit } from '@angular/core';
 import { Petshop } from '../../models/petshop';
 import { Router, NavigationExtras } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -11,36 +13,46 @@ import { Router, NavigationExtras } from '@angular/router';
 })
 export class HomePage implements OnInit {
 
+  user: User;
   type: String = 'shower';
   shower: Petshop;
   cough: Petshop;
 
   constructor(
     private petshopService: PetshopService, 
+    private userService: UserService,
     private router: Router) { 
     }
 
   ngOnInit() {
-    this.getPetshops();
+    this.getAll();
   }
 
-  async getPetshops() {
+  async getAll() {
     try{
-      const { data, ...meta } = await this.petshopService.getPetshops('shower');
+      const { data, ...meta } = await this.petshopService.getAll('shower');
       this.shower = data;
     } catch {
       console.log("Err");
     }
     try{
-      const { data, ...meta } = await this.petshopService.getPetshops('cough');
+      const { data, ...meta } = await this.petshopService.getAll('cough');
       this.cough = data;
+    } catch {
+      console.log("Err");
+    }
+    try{
+      this.user = await this.userService.get();
     } catch {
       console.log("Err");
     }
   }
 
   toPetshop(petshop: Petshop){
-    this.router.navigate(['/petshop'], { state: { petshop } });
+    // this.router.navigate(['/petshop'], { state: { petshop } });
+    this.router.navigate(['/petshop']).then( () =>{
+      this.petshopService.set(petshop);
+    });
   }
 
 }
